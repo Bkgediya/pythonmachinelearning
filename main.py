fe0ca728370b18a0649163c81ba96959f56ecb9b
abc <bhavdipgediya099@gmail.com>
2021-12-05 12:14:49 -0800

    -commit to developer branch

--- main.py
+++ main.py
@@ -9,9 +9,14 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def calculate(a, b):
+    return a + b
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('how are you')
+    print(calculate(5, 6))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
