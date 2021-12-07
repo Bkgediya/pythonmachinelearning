9736deb5a6b2d4fb1a80df5e90b2a5dd6a083e6b
abc <bhavdipgediya099@gmail.com>
2021-12-06 21:19:35 -0800

    new  code is pushed with functionality

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -13,10 +7,23 @@ def calculate(a, b):
     return a - b
 
 
+def calculateMultiply(a, b):
+    return a * b
+
+
+def summation(a, b):
+    return a + b
+
+
+def division(a, b):
+    return a/b
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('how are you')
     print(calculate(5, 6))
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(calculateMultiply(6, 8))
+    print(summation(6, 9))
+    print(division(10, 2))
